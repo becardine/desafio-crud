7a35d0acc5964a2605360e3b557e107247f9974f
becardine <brenda_alves@live.com>
2022-02-08 01:18:06 -0300

    start crud (create)

--- scr/js/crud.js
+++ scr/js/crud.js
@@ -1,2 +1,12 @@
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_course')) ?? []
 const setLocalStorage = (dbCourse) => localStorage.setItem("db_course", JSON.stringify(dbCourse))
+
+
+//CRUD 
+
+//creat
+const createCourse = (course) => {
+    const dbCourse = getLocalStorage()
+    dbCourse.push(course)
+    setLocalStorage(dbCourse)
+}
